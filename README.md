8a07541536597bdb9d6efeddeedb7420aa5b074e
verilye <connor.j.logan@outlook.com>
2023-08-23 16:49:04 +1000

    basic layout

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pretty lookin chat app using C# and React
+
+The goal of this one is to make a good looking chat app that shows mastery
+of the concept of a web socket server. 
--- Server.cs
+++ Server.cs
@@ -1,38 +1,61 @@
 using System.Net.Sockets;
 using System.Net;
-using System;
+using System.Text;
+using System.Text.RegularExpressions;
 
-class Server{
+class Server
+{
+
     // Creating a Web Socket in C#
     // https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_server
 
-    // How a WS server works
+    // Things to consider:
+    // Security, debugging, scaling and load balancing
 
-    // Server listens
-    // We send a handshake in the form of a HTTP GET request
-    // Check the request header for Sec-WebSocket-Key
-    // Validate and upgrade connection to web socket
+    public static void Main(string[] args)
+    {
 
-
-    // Sending messages to server
-
-    // Encoded messages are sent to server
-    // They are decoded
-
-
-    public static void Main(string[] args){
+        // Send a http request to 'get' the connection and then upgrade it to a websocket connection (In C# this is the System.Net.Sockets)
+        // This upgraded connection is a single, two way transmission control Protocol connection
 
         string PORT = "127.0.0.1";
-        TcpListener server = new TcpListener(IPAddress.Parse(PORT),80);
+        // Websocket protocol by default uses port 80 for regular websocket connections
+        TcpListener server = new TcpListener(IPAddress.Parse(PORT), 80);
 
         server.Start();
         Console.WriteLine(
-            "Server has started on " + PORT+":80.{0}Waiting for a connection..."
-            ,Environment.NewLine
+            "Server has started on " + PORT + ":80.{0}Waiting for a connection..."
+            , Environment.NewLine
         );
 
         TcpClient client = server.AcceptTcpClient();
 
-        Console.WritleLine("A client has connected");
+        Console.WriteLine("A client has connected");
+
+        // Open up a Network stream object so that bytes can be pulled from it and checked 
+        // We are looking for a GET command to upgrade the connection
+        NetworkStream stream = client.GetStream();
+
+        while (true)
+        {
+            while(client.Available <3){
+
+            }
+
+            byte[] bytes = new byte[client.Available];
+
+            stream.Read(bytes,0,bytes.Length);
+
+            String data = Encoding.UTF8.GetString(bytes);
+
+            if(Regex.IsMatch(data,"^GET")){
+                // connect and potentially check for authorisation
+            }else{
+                // reject
+            }
+
+
+
+        }
     }
 }
